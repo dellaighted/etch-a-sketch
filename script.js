4b600562332783bac8732fe9dd2c2ca53e353c68
Della Maret <dellamaret01@gmail.com>
2024-08-08 22:50:09 -0400

    implemented drawing feature

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let size = 16;
 
 for (i = 0; i < size * size; i++){
     let cell = document.createElement("div");
-    // console.log(cell.style.width = (500 / size) + "px");
     cell.style.width = (500 / size) + "px"
     cell.style.height = (500 / size) + "px";
     cell.classList.add("cell");
     container.appendChild(cell);
+
+    cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "blue";
+    });
+
 }
