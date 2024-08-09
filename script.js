0eb522c895829c6a6e06c5276d3d6f2c349df65e
Della Maret <dellamaret01@gmail.com>
2024-08-09 15:24:35 -0400

    create new grid function

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const container = document.querySelector(".container");
-
 let size = 16;
+createGrid(size);
 
-for (i = 0; i < size * size; i++){
-    let cell = document.createElement("div");
-    cell.style.width = (500 / size) + "px"
-    cell.style.height = (500 / size) + "px";
-    cell.classList.add("cell");
-    container.appendChild(cell);
-
-    cell.addEventListener("mouseenter", () => {
-        cell.style.backgroundColor = "blue";
-    });
-
+function createGrid(size){
+    if(size >= 2 && size <= 100){
+        for (i = 0; i < size * size; i++){
+            let cell = document.createElement("div");
+            cell.style.width = (500 / size) + "px"
+            cell.style.height = (500 / size) + "px";
+            cell.classList.add("cell");
+            container.appendChild(cell);
+        
+            cell.addEventListener("mouseenter", () => {
+                cell.style.backgroundColor = "blue";
+            });
+        
+        }
+    }else{
+        alert("Please select grid size between 2 and 100.");
+    }
 }
+
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+    for (i = 0; i < size * size; i++){
+        const cell = document.querySelector(".cell");
+        cell.remove();
+    }
+    size = prompt("Enter a new grid size.");
+    createGrid(size);
+});
